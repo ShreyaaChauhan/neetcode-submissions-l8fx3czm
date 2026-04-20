84d865b5f79bb0a5d5f0f5d941381002a0e5619f
Shreya Chauhan <shreyachauhan1113@gmail.com>
2026-04-20 20:21:18 +0200

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    page = int(input())
+    return page
+
+
+def read_float() -> float:
+    temperature = float(input())
+    return temperature
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
